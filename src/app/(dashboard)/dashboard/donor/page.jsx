66adf844552f8741dfe9855a8a66cc3ace858e91
emon-101 <emon26.ugv@gmail.com>
2026-06-22 19:59:37 +0600

    feat: add DonorPage and VolunteerPage components

---
+++ src/app/(dashboard)/dashboard/donor/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DonorPage = () => {
+    return (
+        <div>
+            This is donor page
+        </div>
+    );
+};
+
+export default DonorPage;
---
+++ src/app/(dashboard)/dashboard/volunteer/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VolunteerPage = () => {
+    return (
+        <div>
+            This is volunteer page
+        </div>
+    );
+};
+
+export default VolunteerPage;
